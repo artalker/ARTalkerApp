8bb8024d7c3cb7ed785119673ca25e5c68f23692
Limsoheeee <113952299+Limsoheeee@users.noreply.github.com>
2025-05-24 01:14:28 +0900

    sohee_250524_리액트네이티브 웹뷰연결

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform, SafeAreaView } from "react-native";
+import { WebView as RNWebView } from "react-native-webview";
 
 export default function App() {
+  // 웹뷰 컴포넌트를 플랫폼에 따라 다르게 설정
+  const WebView = Platform.OS === "web" ? "iframe" : RNWebView;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {Platform.OS === "web" ? (
+        <iframe
+          src="https://artalker.netlify.app"
+          // src="http://localhost:3000"
+          style={{ flex: 1, width: "100%", height: "100%" }}
+        />
+      ) : (
+        <RNWebView
+          source={{ uri: "https://artalker.netlify.app" }}
+          // source={{ uri: "http://localhost:3000" }}
+          style={styles.webview}
+        />
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  webview: {
+    flex: 1,
   },
 });
